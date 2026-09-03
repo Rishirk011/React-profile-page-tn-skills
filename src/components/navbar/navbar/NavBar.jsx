23887abae54fba4f33ad9fb6927e4aef21e1d81e
rishi <rishirk253@gmail.com>
2026-09-03 20:02:45 +0530

    project setup done

---
+++ src/components/navbar/navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import styles from '../navbar/navbar.module.css';
+
+const NavBar =()=>{
+
+    return <>
+   
+    </>
+
+}
+
+export default NavBar;
---
+++ src/pages/profile/profile.jsx
@@ -0,0 +1,9 @@
+const ProfileCard = () => {
+
+    return <>
+    
+    </>
+
+}
+
+export default ProfileCard;
